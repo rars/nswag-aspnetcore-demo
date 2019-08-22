f456168293c2a41bc7e1266e10b0a9af822824c9
Richard Russell <richardr39@gmail.com>
2019-08-22 21:18:01 +0100

    feat: add initial NSwag configuration

--- NSwagDemoWebApi/Startup.cs
+++ NSwagDemoWebApi/Startup.cs
@@ -26,6 +26,8 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +45,9 @@ namespace WebApplication1
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
         }
     }
 }
